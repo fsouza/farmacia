244291fae5ed96cdd68db4b1a8a471f950421d9b
Francisco Souza <francisco@franciscosouza.net>
2010-06-23 23:45:58 -0300

    Definição do segundo passo

--- delivery/features/steps.py
+++ delivery/features/steps.py
@@ -2,6 +2,9 @@
 from freshen import *
 from delivery.models import Remedio
 
+from ludibrio import Mock
+from django.test.client import Client
+
 @Given('que existem (\d+) remédios cadastrados no banco de dados')
 def gravar_remedios(quantidade_remedios):
     scc.remedios = []
@@ -9,3 +12,12 @@ def gravar_remedios(quantidade_remedios):
         remedio = Remedio()
         remedio.nome = 'Remédio %d' %(i + 1)
         scc.remedios.append(remedio)
+
+@When('eu vou para a página de listagem de remédios')
+def visitar_pagina_listagem():
+    with Mock() as delivery:
+        from delivery.models import Remedio
+        Remedio.objects.all() > scc.remedios
+    client = Client()
+    scc.response = client.get('/delivery/remedios')
+    delivery.validate()
